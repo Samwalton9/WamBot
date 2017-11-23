00a14b24e4452ca9818d5875b7d2da982f519315
Samwalton9 <swalton@wikimedia.org>
2017-11-23 23:43:26 +0000

    Add log_history(), graph of actions over time

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import asyncio
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime as dt
 
 client = discord.Client()
 
@@ -27,7 +29,8 @@ async def on_message(message):
 async def parse_command(command_string, command_channel):
 
     command_list = {
-        'modlog': modlog_history
+        'hourlyactivity': hourly_activity,
+        'loghistory': log_history
     }
 
     split_command = command_string[1:].split(" ")
@@ -38,6 +41,7 @@ async def parse_command(command_string, command_channel):
         try:
             result = await command_list[primary_command](command_channel, *args)
         except TypeError as e:
+            print(e)
             result = "Error: Wrong number of arguments given"
     else:
         result = "Error: That command doesn't exist"
@@ -45,7 +49,7 @@ async def parse_command(command_string, command_channel):
     if result:
         await client.send_message(command_channel, content=result)
 
-async def modlog_history(command_channel, message_limit):
+async def hourly_activity(command_channel, message_limit):
 
     try:
         limit_int = int(message_limit)
@@ -59,15 +63,14 @@ async def modlog_history(command_channel, message_limit):
     #log_channel = client.get_channel('382550498533703680')  # bot-testing
     log_channel = client.get_channel('302525012143898631')  # mod-log
 
-    message_log = client.logs_from(log_channel, limit=limit_int)
     hours_log = []
 
-    async for historical_message in message_log:
+    async for historical_message in client.logs_from(log_channel, limit=limit_int):
         if historical_message.content.startswith('!'):
             message_hour = historical_message.timestamp.hour
             hours_log.append(message_hour)
 
-    public_log = await hourly_activity()
+    public_log = await user_activity()
 
     f = plt.figure()
 
@@ -93,7 +96,7 @@ async def modlog_history(command_channel, message_limit):
                            'graphs/message_log.png',
                            filename='Modlog history.png')
 
-async def hourly_activity():
+async def user_activity():
     all_channels = client.get_all_channels()
     public_channels = [289466476187090944, 300462993995726848,
                        289468705878966277,
@@ -113,13 +116,64 @@ async def hourly_activity():
         log_channel = client.get_channel(str(channel))
         print("Counting messages in ", log_channel)
 
-        message_log = client.logs_from(log_channel, limit=1000)
-
-        async for historical_message in message_log:
+        async for historical_message in client.logs_from(log_channel, limit=2000):
             message_hour = historical_message.timestamp.hour
             hours_log.append(message_hour)
 
     return hours_log
+
+async def log_history(command_channel, num_days, action=''):
+
+    try:
+        days_int = int(num_days)
+    except ValueError:
+        error_message = "Error: Please use a number."
+        return error_message
+
+    date_subtracted = dt.datetime.today() - dt.timedelta(days=days_int)
+
+    collecting_message = await client.send_message(
+        command_channel, content="Collecting data...")
+
+    log_channel = client.get_channel('302525012143898631')  # mod-log
+    
+    date_log = []
+
+    async for historical_message in client.logs_from(log_channel, after=date_subtracted, limit=20000):
+        if historical_message.content.startswith(('!%s' % action,'.%s' % action)):
+            message_date = historical_message.timestamp
+            date_log.append(message_date)
+
+    if len(date_log) > 0:
+        f = plt.figure()
+        ax = f.add_subplot(111)
+
+        locator = mdates.AutoDateLocator()
+
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(locator))
+
+        plot_args = {'histtype':'step'}
+
+        plt.hist(date_log, color='black', bins=days_int, **plot_args)
+
+        plt.xlabel('Date')
+        plt.ylabel('Number of actions per day')
+
+        f.autofmt_xdate()
+
+        f.set_size_inches(8,5)
+        f.savefig('graphs/ban_log', dpi=300)
+        print("Done analysing messages.")
+
+        await client.edit_message(collecting_message,
+                                  collecting_message.content + " Done.")
+        
+        await client.send_file(command_channel,
+                               'graphs/ban_log.png',
+                               filename='Ban history.png')
+    else:
+        return "No logged actions matched the command."
 
 with open('bot_token') as f:
     token = f.readline().strip()
